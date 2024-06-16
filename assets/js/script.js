9c1e8670263d4d0a9e4b8dd68e2d44f7c144dce6
GauravTendulkar26 <gauravtendulkar1165@gmail.com>
2024-06-16 20:06:36 +0530

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -115,6 +115,26 @@ contactForm.addEventListener('submit' , sendEmail)
 
 
 /*=============== CHANGE BACKGROUND HEADER */
+const scrollHeader = () => {
+  const header = document.getElementById('header')
+  this.scrollY >= 50 ? header.classList.add('bg-header')
+                     : header.classList.remove('bg-header')
 
+}
+
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SCROLL REVEAL ANIMATION */
+
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+})
+
+sr.reveal(`.home__data, .projects__container, .footer__container`)
+sr.reveal(`.home__info div`, {delay: 600, origin: 'bottom', interval: 100})
+sr.reveal(`.skills__content:nth-child(1), .contact__content:nth-child(1)`, { origin: 'right'})
+sr.reveal(`.skills__content:nth-child(2),  .contact__content:nth-child(2)`, { origin: 'left'})
+sr.reveal(`.qualification__content`, { interval: 100})
